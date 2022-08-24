1c6f96f164a30f422f1a5f0cfacced2615e41dcb
Douglas S <94dousch@gafe.molndal.se>
2022-08-24 15:06:57 +0200

    Minor change test

--- frontend/src/pages/AboutUs.jsx
+++ frontend/src/pages/AboutUs.jsx
@@ -6,6 +6,10 @@ function AboutUs() {
       <section className="heading">
         <h1>Om Oss</h1>
         <p>About Us Page.</p>
+        <p>About Us Page.</p>
+        <p>About Us Page.</p>
+        <p>About Us Page.</p>
+        <p>About Us Page.</p>
       </section>
 
     </>
