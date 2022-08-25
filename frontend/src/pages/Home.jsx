15ab0b3b5177d957bf7a60a837e38d8bd3834b53
Douglas S <94dousch@gafe.molndal.se>
2022-08-25 13:49:22 +0200

    Resolving webpage.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { FaQuestionCircle, FaTicketAlt } from "react-icons/fa";
+import ImageSlideShow from '../components/AnimationImg/AnimationImgComp.jsx';
 
 function Home() {
   return (
@@ -7,7 +8,62 @@ function Home() {
       <section className="heading">
         <h1>Hem</h1>
         <p>Välkommen till Kållekärr Samfällighet.</p>
-      </section>
+
+    </section>
+        {/* <div
+      id="carouselExampleIndicators"
+      class="carousel slide"
+      data-ride="carousel"
+    >
+      <ol class="carousel-indicators">
+        <li
+          data-target="#carouselExampleIndicators"
+          data-slide-to="0"
+          class="active"
+        ></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+      </ol> */}
+      {/* <div class="carousel-inner">
+        <div class="carousel-item active">
+          <p>Bild 1</p> */}
+          {/* <img class="d-block w-100" src=".../800x400?auto=yes&bg=777&fg=555&text=First slide" alt="First slide" /> */}
+        {/* </div>
+        <div class="carousel-item">
+          <p>Bild 2</p> */}
+
+          {/* <img class="d-block w-100" src=".../800x400?auto=yes&bg=666&fg=444&text=Second slide" alt="Second slide" /> */}
+        {/* </div>
+        <div class="carousel-item">
+          <p>Bild 3</p> */}
+
+          {/* <img class="d-block w-100" src=".../800x400?auto=yes&bg=555&fg=333&text=Third slide" alt="Third slide" /> */}
+        {/* </div>
+      </div>
+      <a
+        class="carousel-control-prev"
+        href="#carouselExampleIndicators"
+        role="button"
+        data-slide="prev"
+      >
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a
+        class="carousel-control-next"
+        href="#carouselExampleIndicators"
+        role="button"
+        data-slide="next"
+      >
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div> */}
+
+        {/* <div>
+          <ImageSlideShow/>
+        </div> */}
+
 
     </>
   );
