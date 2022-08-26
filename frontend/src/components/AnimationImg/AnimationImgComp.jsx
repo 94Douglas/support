c40c6bdf9aa0284fc7dbd353682f23303c34ff12
Douglas S <94dousch@gafe.molndal.se>
2022-08-26 10:18:20 +0200

    Fixing the images on home page.

--- frontend/src/components/AnimationImg/AnimationImgComp.jsx
+++ frontend/src/components/AnimationImg/AnimationImgComp.jsx
@@ -1,102 +1,24 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import imgOne from '../../images/imgOne.jpg';
+import imgTwo from '../../images/imgTwo.jpg';
+import imgThree from '../../images/imgThree.jpg';
 
 function AnimationImgComp() {
   return (
-    <div
-      id="carouselExampleIndicators"
-      class="carousel slide"
-      data-ride="carousel"
-    >
-      <ol class="carousel-indicators">
-        <li
-          data-target="#carouselExampleIndicators"
-          data-slide-to="0"
-          class="active"
-        ></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-      </ol>
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <p>Bild 1</p>
-          {/* <img class="d-block w-100" src=".../800x400?auto=yes&bg=777&fg=555&text=First slide" alt="First slide" /> */}
-        </div>
-        <div class="carousel-item">
-          <p>Bild 2</p>
-
-          {/* <img class="d-block w-100" src=".../800x400?auto=yes&bg=666&fg=444&text=Second slide" alt="Second slide" /> */}
-        </div>
-        <div class="carousel-item">
-          <p>Bild 3</p>
-
-          {/* <img class="d-block w-100" src=".../800x400?auto=yes&bg=555&fg=333&text=Third slide" alt="Third slide" /> */}
-        </div>
-      </div>
-      <a
-        class="carousel-control-prev"
-        href="#carouselExampleIndicators"
-        role="button"
-        data-slide="prev"
-      >
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a
-        class="carousel-control-next"
-        href="#carouselExampleIndicators"
-        role="button"
-        data-slide="next"
-      >
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src={imgOne} class="imageSlider imageSliderOne d-block w-100" alt="First Image" />
     </div>
-
-    // <div
-    //   id="carouselExampleIndicators"
-    //   class="carousel slide"
-    //   data-ride="carousel"
-    // >
-    //   <ol class="carousel-indicators">
-    //     <li
-    //       data-target="#carouselExampleIndicators"
-    //       data-slide-to="0"
-    //       class="active"
-    //     ></li>
-    //     <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    //     <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-    //   </ol>
-    //   <div class="carousel-inner">
-    //     <div class="carousel-item active">
-    //       <img class="d-block w-100" src="./imgOne.jpg" alt="First slide" />
-    //     </div>
-    //     <div class="carousel-item">
-    //       <img class="d-block w-100" src="./imgTwo.jpg" alt="Second slide" />
-    //     </div>
-    //     <div class="carousel-item">
-    //       <img class="d-block w-100" src="./imgThree.jpg" alt="Third slide" />
-    //     </div>
-    //   </div>
-    //   <a
-    //     class="carousel-control-prev"
-    //     href="#carouselExampleIndicators"
-    //     role="button"
-    //     data-slide="prev"
-    //   >
-    //     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    //     <span class="sr-only">Förra</span>
-    //   </a>
-    //   <a
-    //     class="carousel-control-next"
-    //     href="#carouselExampleIndicators"
-    //     role="button"
-    //     data-slide="next"
-    //   >
-    //     <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    //     <span class="sr-only">Nästa</span>
-    //   </a>
-    // </div>
+    <div class="carousel-item">
+      <img src={imgTwo} class="imageSlider d-block w-100" alt="Second Image" />
+    </div>
+    <div class="carousel-item">
+      <img src={imgThree} class="imageSlider d-block w-100" alt="Third Image" />
+    </div>
+  </div>
+</div>
   );
 }
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -10,59 +10,11 @@ function Home() {
         <p>Välkommen till Kållekärr Samfällighet.</p>
 
     </section>
-        {/* <div
-      id="carouselExampleIndicators"
-      class="carousel slide"
-      data-ride="carousel"
-    >
-      <ol class="carousel-indicators">
-        <li
-          data-target="#carouselExampleIndicators"
-          data-slide-to="0"
-          class="active"
-        ></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-      </ol> */}
-      {/* <div class="carousel-inner">
-        <div class="carousel-item active">
-          <p>Bild 1</p> */}
-          {/* <img class="d-block w-100" src=".../800x400?auto=yes&bg=777&fg=555&text=First slide" alt="First slide" /> */}
-        {/* </div>
-        <div class="carousel-item">
-          <p>Bild 2</p> */}
+    <div className="imageSlider">
+    <ImageSlideShow />
 
-          {/* <img class="d-block w-100" src=".../800x400?auto=yes&bg=666&fg=444&text=Second slide" alt="Second slide" /> */}
-        {/* </div>
-        <div class="carousel-item">
-          <p>Bild 3</p> */}
+    </div>
 
-          {/* <img class="d-block w-100" src=".../800x400?auto=yes&bg=555&fg=333&text=Third slide" alt="Third slide" /> */}
-        {/* </div>
-      </div>
-      <a
-        class="carousel-control-prev"
-        href="#carouselExampleIndicators"
-        role="button"
-        data-slide="prev"
-      >
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a
-        class="carousel-control-next"
-        href="#carouselExampleIndicators"
-        role="button"
-        data-slide="next"
-      >
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
-    </div> */}
-
-        {/* <div>
-          <ImageSlideShow/>
-        </div> */}
 
 
     </>
